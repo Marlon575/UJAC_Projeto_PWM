1cf192f32089095ae70300ff218dec117d33bc0a
Marlon <marlonmuchanga@gmail.com>
2026-04-03 20:48:10 +0100

    Reoganização da estrutura de pastas e melhoria dos projectos

---
+++ Projecto_Verificao_de_ idade/Projeto_JS/script.js
@@ -0,0 +1,22 @@
+const botao    = document.getElementById("botaoVerificar");
+const campo    = document.getElementById("campoIdade");
+const resultado = document.getElementById("resultado");
+
+function verificarIdade() {
+    const valorDigitado = campo.value.trim();
+    const idade = Number(valorDigitado);
+    if (valorDigitado === "" || isNaN(idade)) {
+    alert("Por favor, insira um valor numérico válido.");
+    return;
+}
+if (idade >= 18) {
+    resultado.textContent = "Maior de idade.";
+    resultado.className = "resultado maior";
+} else {
+    resultado.textContent = "Menor de idade.";
+    resultado.className = "resultado menor";
+}
+
+botao.addEventListener("click", verificarIdade);
+
+}
